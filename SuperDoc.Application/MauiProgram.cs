1c507f9eceeca3ae45cb47bb38bae3140079b015
Patrick Frost Sørensen <patrick@frostit.dk>
2023-11-25 13:47:52 +0100

    Initial setup of application and design
    #sup-51

---
+++ SuperDoc.Application/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Maui;
+
+using Microsoft.Extensions.Logging;
+
+namespace SuperDoc.Application;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        MauiAppBuilder builder = MauiApp.CreateBuilder();
+        builder.UseMauiApp<App>()
+        .UseMauiCommunityToolkit() // Initialize the .NET MAUI Community Toolki
+        .ConfigureFonts(fonts =>
+        {
+            fonts.AddFont("Inter-Regular.ttf", "InterRegular");
+            fonts.AddFont("Inter-SemiBold.ttf", "InterSemiBold");
+            fonts.AddFont("Inter-Bold.ttf", "InterBold");
+            fonts.AddFont("Inter-ExtraBold.ttf", "InterExtraBold");
+
+            fonts.AddFont("FluentSystemIcons-Regular.ttf", "FluentSystemIconsRegular");
+            fonts.AddFont("FluentSystemIcons-Filled.ttf", "FluentSystemIconsFilled");
+        });
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
---
+++ SuperDoc.Application/Views/LegalDocumentsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace SuperDoc.Application.Views;
+
+public partial class LegalDocumentsPage : ContentPage
+{
+	public LegalDocumentsPage()
+	{
+		InitializeComponent();
+	}
+}
